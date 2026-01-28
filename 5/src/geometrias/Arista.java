ef7b0ca8a79da39bc99b19b527e3a50af688dc55
upigava10-ctrl <upi.gava10@gmail.com>
2026-01-28 09:31:08 -0600

    Add files via upload

---
+++ 5/src/geometrias/Arista.java
@@ -0,0 +1,43 @@
+package geometrias;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
+import java.awt.*;
+
+public class Arista extends Geometria {
+
+    private final Punto punto1;
+    private final Punto punto2;
+
+    public Arista(Punto punto1, Punto punto2){
+        this.punto1=punto1;
+        this.punto2=punto2;
+    }
+
+    @Override
+    public void pintar(Graphics2D g2d) {
+        int x1= (int) punto1.xScreen();
+        int y1= (int) punto1.yScreen();
+
+        int x2= (int) punto2.xScreen();
+        int y2= (int) punto2.yScreen();
+
+        if (punto1.z()<0 && punto2.z()<0) {
+            return;
+        }
+
+        g2d.setColor(Color.MAGENTA);
+        g2d.drawLine(x1,y1,x2,y2);
+    }
+
+    @Override
+    public void muevete(Vector vector) {
+        punto1.muevete(vector);
+        punto2.muevete(vector);
+    }
+
+    @Override
+    public void gira(double[] angulos, Punto centroGiro) {
+
+    }
+}
---
+++ 5/src/geometrias/Geometria.java
@@ -0,0 +1,24 @@
+package geometrias;
+
+import java.awt.*;
+import java.sql.SQLOutput;
+
+public abstract class Geometria {
+    public abstract void pintar(Graphics2D g2d);
+
+    public abstract void muevete(Vector vector);
+    public abstract void gira(double[] angulos, Punto centroGiro);
+
+    public final void gira(Vector vector, Punto centroGiro){
+        double[] angulos=new double[3];
+        angulos[0]=vector.x();
+        angulos[1]=vector.y();
+        angulos[2]=vector.z();
+
+        gira(angulos,centroGiro);
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
